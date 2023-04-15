9cedf32597292c8dfcf15dd492d0013c8e1fa028
ArturDokhno <artur.dohno@icloud.com>
2023-04-15 12:18:34 +0500

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Артур Дохно on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
